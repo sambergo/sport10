192135b7af16d3aed4517087697e5091de5f5ddd
Bergo <bergo@tutanota.com>
2025-07-12 12:24:21 +0300

    pdev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       "@/common": path.resolve(__dirname, "../common"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
 })

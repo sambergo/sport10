8b2ccec07c04fd11ce80c74ab23f1413d6c8c784
Bergo <bergo@tutanota.com>
2025-07-13 14:36:36 +0300

    gi

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "=== Syncing to linode server ==="
-rsync -av --delete -e ssh --verbose -r . linode:fart10 --exclude=.git/ --exclude=node_modules
-
-echo "=== Restarting Docker containers ==="
-ssh linode "cd ~/fart10 && docker-compose down && docker-compose up --build -d"
